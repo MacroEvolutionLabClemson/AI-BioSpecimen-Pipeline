b6ed8324012ee6c8caf5ccac6e6bbf1d53bea755
Maco Evolution <macrolabclemson@gmail.com>
2025-03-07 17:05:54 -0500

    installed YOLO requirments

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'EvoBioVision',
+    version = '0.0.0',
+    author = 'Syed Shahzad Abdul Wajid',
+    author_email = 'macrolabclemson@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+
+)
